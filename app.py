c4fefec9b070970a87dad1a03104d683f3f0b7fc
CeciliaRuiSun <sunrui7337@gmail.com>
2023-01-04 22:09:35 -0800

    Added pagination file

--- app.py
+++ app.py
@@ -8,31 +8,9 @@ from forms import *
 from auth import AuthError, requires_auth
 from flask_wtf.csrf import CSRFProtect
 from config import setup_db
+from pagination import *
 
-ITEMS_PER_PAGE = 10
-TEMP_COMMENTS_PER_PAGE = 5
-COMMENTS_PER_PAGE = 5
 
-def paginate_items(request):
-    page = request.args.get("page", 1, type=int)
-    current_index = page - 1
-    items = Item.query.order_by(Item.id).limit(
-        ITEMS_PER_PAGE).offset(current_index * ITEMS_PER_PAGE).all()
-    return [item.format() for item in items]
-
-def paginate_temp_comments(request):
-    page = request.args.get("page", 1, type=int)
-    current_index = page - 1
-    comments = Temp_comment.query.order_by(Temp_comment.id).limit(
-        TEMP_COMMENTS_PER_PAGE).offset(current_index * TEMP_COMMENTS_PER_PAGE).all()
-    return [comment.format() for comment in comments]
-
-def paginate_comments(request):
-    page = request.args.get("page", 1, type=int)
-    current_index = page - 1
-    comments = Comment.query.order_by(Comment.id).limit(
-        COMMENTS_PER_PAGE).offset(current_index * COMMENTS_PER_PAGE).all()
-    return [comment.format() for comment in comments]
 
 
 def create_app(test_config=None):
